8ae85dc9ec76748f5bf03024078c951d5e31e13f
Nate Pfau <natepfau@yahoo.com>
2021-09-12 09:33:01 -0500

    Added conditionally rendering for Contact

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import About from './components/About';
 import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
-function App() {
+function App()
+{
   const [categories] = useState([
     {
       name: 'commercial',
@@ -16,6 +17,7 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -23,13 +25,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <ContactForm></ContactForm>
-        <Gallery 
-          currentCategory={currentCategory}>
-        </Gallery>
-        <About></About>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
